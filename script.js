d8c5d99f1c968f57bb8b9497afa81b237f7a3538
L-Lament <DevonTech13@gmail.com>
2024-02-11 00:42:39 -0500

    refactored some of the code and added in the reset code to clear the grid

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
+const gridSize = 400
 const sketchArea = document.getElementById('sketchArea')
-const gridSize = 600
 sketchArea.style.width = `${gridSize}px`
 sketchArea.style.height = `${gridSize}px`
+let cells = prompt('Choose your starting size')
 
-let row = 16
-let col = 16
-
-
-for (let i = 0; i < (row * col); i++) {
-    const gridCell = document.createElement('div');
-    gridCell.id = 'cell'
-    gridCell.style.width = `${gridSize/row -2}px`
-    gridCell.style.height = `${gridSize/row -2}px`
-    sketchArea.appendChild(gridCell)
+//make grid
+function makeGrid (cells) {
+    for (let i = 0; i < (cells * cells); i++) {
+        const gridCell = document.createElement('div');
+        gridCell.id = 'cell'
+        gridCell.className = 'empty'
+        gridCell.style.width = `${gridSize/cells - 2}px`
+        gridCell.style.height = `${gridSize/cells - 2}px`
+        sketchArea.appendChild(gridCell)
+    }
 }
+makeGrid(cells)
 
-const singleCell = document.querySelector('.gridCell')
+//make new grid with user input
 
-const hoveredCell = document.querySelectorAll('#cell')
 
-hoveredCell.forEach(hoveredCell => {
-    hoveredCell.addEventListener('mouseover', () => {
-        hoveredCell.setAttribute('class', 'filled')
+//If cell is hovered over, change background color to black
+const cell = document.querySelectorAll('#cell')
+const checkHover = () => {
+    cell.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('class', 'filled')
+        })
+    });
+    
+}
+checkHover()
+
+//Make reset button to clear filled cells
+const resetButton = document.getElementById('resetButton')
+const resetGrid = () => {
+    resetButton.addEventListener('click', () => {
+        cell.forEach(cell =>{
+            cell.setAttribute('class', 'empty')
+        })
     })
-})
+    // makeGrid(cells)
+}
+resetGrid()
 
-//another way to make it work
-// for (let i = 0; i < row; i++) {
-//     const gridCell = document.createElement('div');
-//     gridCell.className = 'gridCell'
-//     gridCell.style.width = `${gridSize/row -2}px`
-//     gridCell.style.height = `${gridSize/row -2}px`
-//     sketchArea.appendChild(gridCell)
-    
-    
-//     for (let j = 1; j < col; j++) {
-//         const gridCell = document.createElement('div');
-//         gridCell.className = 'gridCell'
-//         gridCell.style.width = `${gridSize/row -2}px`
-//         gridCell.style.height = `${gridSize/row -2}px`
-//         sketchArea.appendChild(gridCell)
-        
-        
-//     }
-// }
+
 
 
