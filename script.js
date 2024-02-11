c9e6ec40affa83749ef3945a5165362ae039f873
L-Lament <DevonTech13@gmail.com>
2024-02-10 20:27:53 -0500

    made divs that are hovered switch class to filled

--- script.js
+++ script.js
@@ -3,15 +3,46 @@ const gridSize = 600
 sketchArea.style.width = `${gridSize}px`
 sketchArea.style.height = `${gridSize}px`
 
-let row = 20
-let col = 20
+let row = 16
+let col = 16
 
 
 for (let i = 0; i < (row * col); i++) {
     const gridCell = document.createElement('div');
-    gridCell.className = 'gridCell'
+    gridCell.id = 'cell'
     gridCell.style.width = `${gridSize/row -2}px`
     gridCell.style.height = `${gridSize/row -2}px`
     sketchArea.appendChild(gridCell)
 }
-console.log(gridCell.width);
+
+const singleCell = document.querySelector('.gridCell')
+
+const hoveredCell = document.querySelectorAll('#cell')
+
+hoveredCell.forEach(hoveredCell => {
+    hoveredCell.addEventListener('mouseover', () => {
+        hoveredCell.setAttribute('class', 'filled')
+    })
+})
+
+//another way to make it work
+// for (let i = 0; i < row; i++) {
+//     const gridCell = document.createElement('div');
+//     gridCell.className = 'gridCell'
+//     gridCell.style.width = `${gridSize/row -2}px`
+//     gridCell.style.height = `${gridSize/row -2}px`
+//     sketchArea.appendChild(gridCell)
+    
+    
+//     for (let j = 1; j < col; j++) {
+//         const gridCell = document.createElement('div');
+//         gridCell.className = 'gridCell'
+//         gridCell.style.width = `${gridSize/row -2}px`
+//         gridCell.style.height = `${gridSize/row -2}px`
+//         sketchArea.appendChild(gridCell)
+        
+        
+//     }
+// }
+
+
